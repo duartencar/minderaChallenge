a8678f208115b008664caeb8ee4d740b9423f11d
Duarte Carvalho <up201503661@fe.up.pt>
2018-05-03 13:22:07 +0100

    finds groups correctly but consumes all memory

--- src/minderaChallenge/AdjacentCellsChallenge.java
+++ src/minderaChallenge/AdjacentCellsChallenge.java
@@ -4,12 +4,15 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class AdjacentCellsChallenge {
 	
 	private static ArrayList<ArrayList<Character>> grid = new ArrayList<ArrayList<Character>>();
 	private static boolean[][] visitedCells;
-	private static ArrayList<Integer[]> exploredCells;
+	private static Integer[] coordinates = new Integer[2];
+	private static ArrayList<Integer[]> exploredCells = new ArrayList<Integer[]>();
+	private static ArrayList<ArrayList<Integer[]>> groups = new ArrayList<ArrayList<Integer[]>>();
 	private static boolean exploring = false;
 	
 	
@@ -31,6 +34,14 @@ public class AdjacentCellsChallenge {
 		System.out.println("Read " + grid.size() * grid.get(0).size() + " grid elements in " + 1.0 * duration / 1000 + " seconds.");
 			
 		updateVisited();
+		
+		startTime = System.currentTimeMillis();
+		findGroups();
+		endTime = System.currentTimeMillis();
+		
+		System.out.println("Gathered groups in " + 1.0 * duration / 1000 + " seconds.");
+		
+		//printGroups();
 	}
 	
 	private static void findGroups() {
@@ -41,30 +52,90 @@ public class AdjacentCellsChallenge {
 				if(!visitedCells[y][x] && grid.get(y).get(x) == '1') {
 					exploring = true;
 					explore(y,x);
+					
+					if(exploredCells.size() > 1) {
+						groups.add((ArrayList<Integer[]>) exploredCells.clone());
+					}
+					
+					exploredCells.clear();
+					
+					exploring = false;
 				}
 			}
+		}
+	}
+	
+	private static void printCoord(Integer[] coords) {
+		System.out.format("[%d,%d]", coords[0], coords[1]);
+	}
+	
+	private static void printGroup(int groupIndex) {
+		
+		System.out.print("[ ");
+		
+		for(int i = 0; i < groups.get(groupIndex).size(); i++) {
+			printCoord(groups.get(groupIndex).get(i));
+			
+			if(i != groups.get(groupIndex).size() - 1) {
+				System.out.print(", ");
+			}
+		}
+		
+		System.out.print(" ]\n");
+	}
+	
+	private static void printGroups() {
+		
+		for(int i = 0; i < groups.size(); i++) {
+			Collections.reverse(groups.get(i));
+			printGroup(i);
 		}
 	}
 	
 	private static void explore(int y, int x) {
 		visitedCells[y][x] = true;
 		
-		if(!visitedCells[y+1][x] && y+1 < grid.size() && grid.get(y+1).get(x) == '1') {
-			explore(y+1, x);
+		boolean hasNeighboor = false;
+		
+		if(y+1 < grid.size() && grid.get(y+1).get(x) == '1') {
+			if(!visitedCells[y+1][x]) {
+				explore(y+1, x);
+			}
+			
+			hasNeighboor = true;
 		}
-		if(!visitedCells[y-1][x] && y-1 > 0 && grid.get(y-1).get(x) == '1') {
-			explore(y-1, x);
+		if(y-1 > 0 && grid.get(y-1).get(x) == '1') {
+			if(!visitedCells[y-1][x]) {
+				explore(y-1, x);
+			}
+			
+			hasNeighboor = true;
 		}
-		if(!visitedCells[y][x-1] && x-1 > 0 && grid.get(y).get(x-1) == '1') {
-			explore(y, x-1);
+		if(x-1 > 0 && grid.get(y).get(x-1) == '1') {
+			if(!visitedCells[y][x-1]) {
+				explore(y, x-1);
+			}
+			
+			hasNeighboor = true;
 		}
-		if(!visitedCells[y][x+1] && x+1 > grid.size() && grid.get(y).get(x+1) == '1') {
-			explore(y, x+1);
+		if(x+1 < grid.get(y).size() && grid.get(y).get(x+1) == '1') {
+			if(!visitedCells[y][x+1]) {
+				explore(y, x+1);
+			}
+			
+			hasNeighboor = true;
+		}
+		
+		if(hasNeighboor) {
+			coordinates[0] = y;
+			coordinates[1] = x;
+			exploredCells.add(coordinates.clone());
 		}
 	}
 	
+	
 	private static void updateVisited() {
-		visitedCells = new boolean [grid.size()][grid.size()];
+		visitedCells = new boolean [grid.size()][grid.get(0).size()];
 		
 		for(boolean[] line : visitedCells) {
 			for(boolean element : line) {
