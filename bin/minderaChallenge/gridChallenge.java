4a77946353585d4d20a87594bf9b4fa73629e25d
Duarte Carvalho <up201503661@fe.up.pt>
2018-05-03 11:03:29 +0100

    added java file

---
+++ bin/minderaChallenge/gridChallenge.java
@@ -0,0 +1,14 @@
+package minderaChallenge;
+
+import java.util.ArrayList;
+
+public class gridChallenge {
+
+	ArrayList<ArrayList<Character>> grid = new ArrayList<ArrayList<Character>>();
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
